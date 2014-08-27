52fbc6f86672a90ce6504e93c53364179baa7455
Simcha Kanter <simchakanter@gmail.com>
2014-08-27 12:58:56 -0500

    Add route change animations

--- app/cnc-app.js
+++ app/cnc-app.js
@@ -20,8 +20,23 @@ cncApp.config(function($routeProvider) {
       }
     }
   })
+  // TODO add an error page
   .otherwise({
     redirectTo: '/'
+  });
+});
+
+cncApp.run(function($rootScope, $location, $timeout) {
+  $rootScope.$on('$routeChangeError', function() {
+      $location.path("/error");
+  });
+  $rootScope.$on('$routeChangeStart', function() {
+      $rootScope.isLoading = true;
+  });
+  $rootScope.$on('$routeChangeSuccess', function() {
+    $timeout(function() {
+      $rootScope.isLoading = false;
+    }, 1000);
   });
 });
 
